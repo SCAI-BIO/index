d93a0b7b815938bac52dfc1903cdaba884dcab2b
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2024-07-09 12:36:48 +0200

    refactor: fix the tests

--- client/src/app/mappings-table/mappings-table.component.spec.ts
+++ client/src/app/mappings-table/mappings-table.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MappingsTableComponent } from './mappings-table.component';
 
 describe('MappingsTableComponent', () => {
@@ -8,9 +8,8 @@ describe('MappingsTableComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MappingsTableComponent]
-    })
-    .compileComponents();
+      imports: [BrowserAnimationsModule, MappingsTableComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(MappingsTableComponent);
     component = fixture.componentInstance;
