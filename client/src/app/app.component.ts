95ccf6d4c06f461defa9fe697412bed9227dccbb
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2024-10-15 10:41:30 +0200

    refactor: replace html elements with route links

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,10 +3,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from "./footer/footer.component";
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule, MatMenuModule, RouterLink],
+  imports: [RouterOutlet, MatButtonModule, MatMenuModule, RouterLink, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
