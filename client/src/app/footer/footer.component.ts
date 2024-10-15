3377280585bbe1ae969c8d3d8743017e4abc1673
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2024-10-15 10:40:45 +0200

    add: footer component

---
+++ client/src/app/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [RouterModule, MatIconModule, MatListModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
+})
+export class FooterComponent {}
