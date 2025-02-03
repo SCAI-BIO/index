7e09a1aaee11e7d3a83af7d77e9d8faece428ad3
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2025-02-03 17:04:02 +0100

    refactor: assign path to components

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,11 +1,21 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { QueryComponent } from './query/query.component';
+import { HarmonizeComponent } from './harmonize/harmonize.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     pathMatch: 'full',
+  },
+  {
+    path: 'query',
+    component: QueryComponent,
+  },
+  {
+    path: 'harmonize',
+    component: HarmonizeComponent,
   },
   {
     path: '**',
