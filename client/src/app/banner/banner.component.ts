34cb918945768120614ebf12114366d96bdbacac
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2025-02-03 17:04:25 +0100

    refactor: change logo and background color

--- client/src/app/banner/banner.component.ts
+++ client/src/app/banner/banner.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-banner',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './banner.component.html',
-  styleUrl: './banner.component.scss'
+  styleUrl: './banner.component.scss',
 })
-export class BannerComponent {
-
-}
+export class BannerComponent {}
