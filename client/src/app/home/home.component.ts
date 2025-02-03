b109d1534ec3d701aaf485b8f07abda3966277a3
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2025-02-03 17:05:35 +0100

    refactor: prototype home page

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, RouterModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
