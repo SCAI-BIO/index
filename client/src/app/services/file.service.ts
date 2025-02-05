42944e7f2556e9c00fda7774eab4ea91552320bd
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2025-02-05 17:29:56 +0100

    feat: create a file service

---
+++ client/src/app/services/file.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { Response } from '../interfaces/mapping';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FileService {
+  constructor() {}
+
+  convertToCSV(data: Response[]): string {
+    const headers = [
+      'Similarity',
+      'Variable',
+      'Description',
+      'ConceptID',
+      'PrefLabel',
+    ];
+
+    const escapeCSV = (value: string) => `"${value.replace(/"/g, '""')}"`;
+    const rows = data.map((item) =>
+      [
+        item.mappings[0].similarity,
+        escapeCSV(item.variable),
+        escapeCSV(item.description),
+        escapeCSV(item.mappings[0].concept.id),
+        escapeCSV(item.mappings[0].concept.name),
+      ].join(',')
+    );
+    return [headers.join(','), ...rows].join('\n');
+  }
+
+  async downloadCSV(
+    data: Response[],
+    suggestedFileName: string
+  ): Promise<void> {
+    const csvData = this.convertToCSV(data);
+    const blob = new Blob([csvData], { type: 'text/csv' });
+
+    // Extend TypeScript to recognize showSaveFilePicker
+    if ('showSaveFilePicker' in window) {
+      try {
+        const saveFilePicker = window.showSaveFilePicker;
+        if (typeof saveFilePicker === 'function') {
+          const handle = await saveFilePicker({
+            suggestedName: suggestedFileName,
+            types: [
+              {
+                description: 'CSV file',
+                accept: { 'text/csv': ['.csv'] },
+              },
+            ],
+          });
+
+          const writable = await handle.createWritable();
+          await writable.write(blob);
+          await writable.close();
+          return;
+        }
+      } catch (error) {
+        console.error('File saving canceled or failed:', error);
+      }
+    }
+  }
+}
