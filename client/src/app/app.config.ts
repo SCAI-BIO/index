545bdfa938fbee70945a5c4a661329f633cd0d4d
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2025-04-28 09:21:46 +0200

    refactor: use hash location strategy

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
@@ -7,7 +7,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideAnimationsAsync(),
     provideHttpClient(),
   ],
