7901d8a77c36a2d822d412758339710ffd234199
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2025-05-02 18:30:03 +0200

    minor changes

--- api/app/dependencies.py
+++ api/app/dependencies.py
@@ -4,3 +4,7 @@ from app.models import WeaviateClient
 async def get_client():
     with WeaviateClient() as client:
         yield client
+
+
+def get_client_instance() -> WeaviateClient:
+    return WeaviateClient()
